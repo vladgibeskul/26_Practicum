275cceec32f662393a409995f3b8c3811454a732
vladgibeskul <wgv96300@gmail.com>
2025-02-19 08:48:32 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Value();
+            Console.ReadKey();
+
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static void Value()
+        {
+            int n = Input("Введите числа:");
+            int[] array = new int[n];
+
+            Console.WriteLine("Введите элементы массива:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Элемент {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Вывод массива:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine(); 
+
+        }
+    }
+}
