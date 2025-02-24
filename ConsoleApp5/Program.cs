5bfffa24ea5350cd7dee3aa72dc8d6112e842c13
vladgibeskul <wgv96300@gmail.com>
2025-02-24 22:50:57 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Array(10); 
+
+            Print(array); 
+        }
+
+        
+        static int[] Array(int size)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i; 
+            }
+
+            return array; 
+        }
+
+       
+        static void Print(int[] array)
+        {
+            Console.WriteLine("Элементы массива:");
+            foreach (int element in array)
+            {
+                Console.Write(element + " "); 
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
